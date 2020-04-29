1754961630c063c91ec4f1b27fd895906f5e44de
Oguzhan <oguzhanturgut@outlook.com>
2020-04-29 18:07:37 +0200

    add dynamic navigation options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -22,37 +22,37 @@ const CategoriesScreen = (props) => {
           });
         }}>
           <View>
-          <Text>
-        {itemData.item.title}
-          </Text>
+            <Text>
+              {itemData.item.title}
+            </Text>
           </View>
-          </TouchableOpacity>
-          );
-          };
+        </TouchableOpacity>
+    );
+  };
 
-          return <FlatList data={CATEGORIES} renderItem={renderGridItem}
-          numColumns={2}/>;
-          };
+  return <FlatList data={CATEGORIES} renderItem={renderGridItem}
+                   numColumns={2}/>;
+};
 
-          CategoriesScreen.navigationOptions = {
-          headerTitle:'Meal Categories',
-          headerStyle:{
-          backgroundColor: Platform.OS === 'android'? Colors.primaryColor : '',
-          },
-          headerTintColor:Platform.OS === 'android'? 'white' : Colors.primaryColor,
-          };
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+};
 
-          const styles = StyleSheet.create({
-          screen: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          },
-          gridItem: {
-          flex: 1,
-          margin: 15,
-          height: 150,
-          },
-          });
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
 
-          export default CategoriesScreen;
+export default CategoriesScreen;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React from "react";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
 
-import {CATEGORIES} from '../data/dummy-data';
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealScreen = (props) => {
-  const catId = props.navigation.getParam('categoryId');
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return (
-      <View styles={styles.screen}>
-        <Text> Category Meal Screen </Text>
-        <Text>{selectedCategory.title}</Text>
-        <Button title={'Go to Meal Details'} onPress={() => {
-          props.navigation.navigate({routeName:'MealDetail'});
-        }}/>
-        <Button title={'Go Back'} onPress={()=>{
+    <View styles={styles.screen}>
+      <Text> Category Meal Screen </Text>
+      <Button
+        title={"Go to Meal Details"}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      />
+      <Button
+        title={"Go Back"}
+        onPress={() => {
           props.navigation.goBack();
           // props.navigation.pop();
-        }} />
-      </View>
+        }}
+      />
+    </View>
   );
 };
 
-CategoryMealScreen.navigationOptions = {};
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
