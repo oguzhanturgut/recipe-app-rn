a7d2043dc43bd160108303775599e4132db85cd5
Oguzhan <oguzhanturgut@outlook.com>
2020-04-30 00:53:42 +0200

    grid styling & refactoring

--- App.js
+++ App.js
@@ -6,20 +6,21 @@ import MealsNavigator from './navigation/MealsNavigator';
 
 enableScreens();
 
-const fetchFonts = ()=>{
+const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
-}
+};
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
   if (!fontLoaded) {
-    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}/>;
+    return <AppLoading startAsync={fetchFonts}
+                       onFinish={() => setFontLoaded(true)}/>;
   }
   return (
-    <MealsNavigator />
+      <MealsNavigator/>
   );
 }
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,54 +1,41 @@
-import React from "react";
+import React from 'react';
 import {
-  View,
-  Text,
-  TouchableOpacity,
   FlatList,
   StyleSheet,
-} from "react-native";
+} from 'react-native';
 
-import { CATEGORIES } from "../data/dummy-data";
+import {CATEGORIES} from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+        <CategoryGridTile title={itemData.item.title}
+                          color={itemData.item.color} onSelect={() => {
           props.navigation.navigate({
-            routeName: "CategoryMeals",
+            routeName: 'CategoryMeals',
             params: {
               categoryId: itemData.item.id,
             },
           });
-        }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+        }}/>
     );
   };
 
   return (
-    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+      <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
   );
 };
 
 CategoriesScreen.navigationOptions = {
-  headerTitle: "Meal Categories",
+  headerTitle: 'Meal Categories',
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
