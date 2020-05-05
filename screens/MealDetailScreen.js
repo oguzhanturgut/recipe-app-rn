0656b213cb58ef5ab6f8642cb33216ea3a2cc5a9
Oguzhan <oguzhanturgut@outlook.com>
2020-05-05 18:54:50 +0200

    add fav header icon

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 import {MEALS} from '../data/dummy-data';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
@@ -22,6 +25,11 @@ MealDetailScreen.navigationOptions = navigationData => {
 
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+      <Item title={'Favorite'} iconName={'ios-star'} onPress={() => {
+        console.log('Mark as favorite');
+      }}/>
+    </HeaderButtons>,
   };
 };
 
