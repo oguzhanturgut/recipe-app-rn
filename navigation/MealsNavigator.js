7bef324751f53d9eab89facc47ea2a7fb61f729f
Oguzhan <oguzhanturgut@outlook.com>
2020-05-06 16:36:37 +0200

    add materialTabNavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import {Platform} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import {Ionicons} from '@expo/vector-icons';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealScreen from '../screens/CategoryMealsScreen';
@@ -30,4 +35,57 @@ const MealsNavigator = createStackNavigator(
     },
 );
 
-export default createAppContainer(MealsNavigator);
+const tabScreenConfig = {
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Meals',
+      tabBarIcon: (tabInfo) => {
+        return (
+            <Ionicons
+                name="ios-restaurant"
+                size={25}
+                color={tabInfo.tintColor}
+            />
+        );
+      },
+      tabBarColor:Colors.primaryColor
+    },
+  },
+  Favorites: {
+    screen: FavoritesScreen, navigationOptions: {
+      tabBarLabel: 'Favorites',
+      tabBarIcon: (tabInfo) => {
+        return (
+            <Ionicons
+                name="ios-star"
+                size={25}
+                color={tabInfo.tintColor}
+            />
+        );
+      },
+      tabBarColor:Colors.accentColor
+    },
+  },
+};
+
+const MealsFavTabNavigator = Platform.OS === 'android' ?
+    createMaterialBottomTabNavigator(tabScreenConfig, {
+      activeColor:'white',
+      shifting:true,
+      barStyle:{
+        backgroundColor: Colors.primaryColor
+      },
+      // swipeEnabled:true
+    }) :
+    createBottomTabNavigator(
+        tabScreenConfig,
+        {
+          tabBarOptions: {
+            activeTintColor: Colors.accentColor,
+          },
+          // swipeEnabled:true
+        },
+    );
+
+export default createAppContainer(MealsFavTabNavigator);
